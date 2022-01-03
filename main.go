ed2e6aae292e61c6a531365424a08ca9e502ef56
Jay Gudsson <gudsson@gmail.com>
2022-01-02 22:28:25 -0800

    cleans up comments

--- main.go
+++ main.go
@@ -14,14 +14,11 @@ import (
 )
 
 func main() {
-
-
-	events := getData()
-
-	fmt.Println(events[6])
+	events := getEvents()
+	fmt.Println(events)
 }
 
-func getData() []model.Event {
+func getEvents() []model.Event {
 	c := colly.NewCollector(
 		colly.AllowedDomains("www.nhl.com"),
 	)
@@ -56,62 +53,9 @@ func getData() []model.Event {
 		}
 		
 		eventArr = append(eventArr, event)
-		// fmt.Println(events)
 	})
-
-	// fmt.Println(events)
 
 	c.Visit("http://www.nhl.com/scores/htmlreports/20212022/PL020562.HTM") 
 
 	return eventArr
 }
-
-// func awaitTask() <-chan int32 {
-// 	r := make(chan int32)
-
-// 	go func() {
-// 		defer close(r)
-
-
-// 	}
-
-// 	c := colly.NewCollector(
-// 			colly.AllowedDomains("www.nhl.com"),
-// 	)
-
-// 	events := []model.Event{}
-
-// 	c.OnHTML("tr[id^='PL-']", func(e *colly.HTMLElement) {
-// 		tds := []*goquery.Selection{}
-
-// 		e.DOM.Children().Each(func(i int, s *goquery.Selection) {
-// 			tds = append(tds, s)
-// 		})
-
-		
-// 		eventId, _ := strconv.Atoi(tds[0].Text())
-// 		periodNum, _ := strconv.Atoi(tds[1].Text())
-// 		periodTimes := utils.PeriodClock(tds[3].Text())
-
-// 		awayPlayers := utils.PlayersOnIce(tds[6])
-// 		homePlayers := utils.PlayersOnIce(tds[7])
-
-// 		event := model.Event{
-// 			EventId: int16(eventId),
-// 			Period: int8(periodNum),
-// 			Strength: tds[2].Text(),
-// 			PeriodTimeElapsed: periodTimes[0],
-// 			PeriodTimeRemaining: periodTimes[1],
-// 			EventCode: tds[4].Text(),
-// 			EventDescription: tds[5].Text(),
-// 			AwayOnIce: awayPlayers,
-// 			HomeOnIce: homePlayers,
-// 		}
-		
-// 		events = append(events, event)
-// 	})
-
-// 	// fmt.Println(events)
-
-// 	c.Visit("http://www.nhl.com/scores/htmlreports/20212022/PL020562.HTM") 
-// }
